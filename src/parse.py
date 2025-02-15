f46b3d0964b4eed963d7aafbc2d4e3dd8ba8509a
rm-a0 <michalrepcik9@gmail.com>
2025-02-15 22:23:54 +0100

    added all tokens to lexer

--- src/parse.py
+++ src/parse.py
@@ -3,7 +3,7 @@
 import sys
 import re
 
-# Error returns
+# Error types
 MISSING_PARAM = 10
 ERROR_STDIN = 11
 ERROR_STDOUT = 12
@@ -15,14 +15,48 @@ SEMANTIC_ERROR_MISSMATCH = 33
 SEMANTIC_ERROR_VAR_COLLISION = 34
 INTERNAL_ERROR = 99
 
-TOKENS = [
-    ("CLASS_T", r'class'),
-    ("SELF_T", r'self'),
-    ("SUPER_T", r'super'),
-    ("NIL_T", r'nil'),
-    ("TRUE_T", r'true'),
-    ("FALSE_T", r'false'),
+# Regex for tokens
+TOKEN_REG = [
+    # Reserved keywords
+    ("CLASS_KW", r'class'),
+    ("SELF_KW", r'self'),
+    ("SUPER_KW", r'super'),
+    ("NIL_KW", r'nil'),
+    ("TRUE_KW", r'true'),
+    ("FALSE_KW", r'false'),
+    # Builtin classes
+    ("OBJECT_BC", r'Object'),
+    ("NIL_BC", r'Nil'),
+    ("TRUE_BC", r'True'),
+    ("FALSE_BC", r'False'),
+    ("INT_BC", r'Integer'),
+    ("STRING_BC", r'String'),
+    ("BLOCK_BC", r'Block'),
+    # Identifiers
+    ("IDENTIFIER", r'[a-z_][a-zA-Z0-9_]*'),
+    ("CLASS_IDENTIFIER", r'[A-Z][a-zA-Z0-9]*'),
+    # Other tokens
+    ("ASSIGN", r':='),
+    ("DOT", r'\.'),
+    ("COLON", r':'),
+    ("L_BRACE", r'\{'),
+    ("R_BRACE", r'\}')
+    ("L_BRACKET", r'\['),
+    ("R_BRACKET", r'\]'),
+    ("PIPE", r'\|'),
+    ("OPERATOR", r'[+\-*/]'),
+    ("STRING", r"'([^'\\]*(\\['n\\][^'\\]*)*)'"),
+    ("INTEGER", r'[+-]?\d+'),
+    ("MISMATCH", r'.'),
 ]
+
+# Reserved keywords to double check Identifiers
+RESERVED_KEYWORDS = {
+    "class", "self", "super", "nil", "true", "false",
+    "Object", "Nil", "True", "False", "Integer", "String", "Block"
+}
+
+
 class Lexer:
     def __init__(self):
         self.tokens = 0
