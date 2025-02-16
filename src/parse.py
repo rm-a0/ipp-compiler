e51890cc3c1968df5b7a5b046ee4eeab803e8f11
rm-a0 <michalrepcik9@gmail.com>
2025-02-16 16:41:18 +0100

    finished lexer and tested on simple example

--- src/parse.py
+++ src/parse.py
@@ -16,7 +16,7 @@ SEMANTIC_ERROR_VAR_COLLISION = 34
 INTERNAL_ERROR = 99
 
 # Regex for tokens
-TOKEN_REG = [
+TOKEN_TOUPLE_ARR = [
     # Reserved keywords
     ("CLASS_KW", r'class'),
     ("SELF_KW", r'self'),
@@ -40,15 +40,24 @@ TOKEN_REG = [
     ("DOT", r'\.'),
     ("COLON", r':'),
     ("L_BRACE", r'\{'),
-    ("R_BRACE", r'\}')
+    ("R_BRACE", r'\}'),
     ("L_BRACKET", r'\['),
     ("R_BRACKET", r'\]'),
     ("PIPE", r'\|'),
     ("OPERATOR", r'[+\-*/]'),
     ("STRING", r"'([^'\\]*(\\['n\\][^'\\]*)*)'"),
     ("INTEGER", r'[+-]?\d+'),
-    ("MISMATCH", r'.'),
+    # Whitespace and comments 
+    ("WHITESPACE", r'[ \t]+'),
+    ("NEWLINE", r'\n'),
+    ("COMMENT", r'".*?"'),
+    # Invalid token
+    ("INVALID", r'.')
 ]
+
+# Combine regex patterns
+TOKEN_REGEX = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in TOKEN_TOUPLE_ARR)
+get_token = re.compile(TOKEN_REGEX).match
 
 # Reserved keywords to double check identifiers
 RES_KEYWORDS = {
@@ -56,20 +65,60 @@ RES_KEYWORDS = {
     "Object", "Nil", "True", "False", "Integer", "String", "Block"
 }
 
+# Token class
+class Token: 
+    def __init__(self, token_type, value = None):
+        self.type = token_type
+        self.value = value
+    
+    def __repr__(self):
+        return f"Token(type: {self.type!r}, value: {self.value!r})"
 
+# Lexer class
 class Lexer:
-    def __init__(self):
-        self.tokens = 0
+    def __init__(self, src_code):
+        self.src_code = src_code
+        self.tokens = []
 
+    # Populate token array in Lexer
     def tokenize(self):
-        return
+        idx = 0
+        while idx < len(self.src_code):
+            match = get_token(self.src_code, idx)
+            if not match:
+                sys.exit(LEXICAL_ERROR)
 
+            token_type = match.lastgroup
+            # Skip whitespace
+            if token_type in {"WHITESPACE", "NEWLINE"}:
+                pass
+            # Create token
+            else:
+                if token_type in {"IDENTIFIER", "CLASS_IDENTIFIER", "OPERATOR", "STRING", "INTEGER"}:
+                    token = Token(token_type, match.group(token_type))
+                else:
+                    token = Token(token_type)
+                self.tokens.append(token)
+
+            idx = match.end()
+
+# Parser class
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
+        self.state = "START"
+
+    def parse(self):
+        for token in self.tokens:
+            print(token)
 
 def main():
-    lexer = Lexer()
+    src_code = sys.stdin.read()
+    lexer = Lexer(src_code)
+    lexer.tokenize()
+
+    parser = Parser(lexer.tokens)
+    parser.parse()
 
 if __name__ == "__main__":
     main()
