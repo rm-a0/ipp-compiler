2e30f3d497f2534de8402d555dd362c90e83caa7
rm-a0 <michalrepcik9@gmail.com>
2025-03-09 21:25:02 +0100

    updeated build

--- test/build.py
+++ test/build.py
@@ -8,6 +8,7 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 # Define the paths for the files
 src_file = os.path.join(current_dir, '..', 'src', 'parse.py')
 doc_file = os.path.join(current_dir, '..', 'doc', 'readme1.md')
+ext = os.path.join(current_dir, '..', 'doc', 'rozsireni')
 
 zip_filename = os.path.join(current_dir, 'xrepcim00.zip')
 
@@ -15,3 +16,4 @@ zip_filename = os.path.join(current_dir, 'xrepcim00.zip')
 with zipfile.ZipFile(zip_filename, 'w') as zipf:
     zipf.write(src_file, os.path.basename(src_file))
     zipf.write(doc_file, os.path.basename(doc_file))
+    zipf.write(ext, os.path.basename(ext))
