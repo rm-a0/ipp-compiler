8050b67530b88cb3b66021d6eb3a64d63df83958
rm-a0 <michalrepcik9@gmail.com>
2025-03-29 23:03:46 +0100

    feat: add xml check and load into interpreter

--- src/interpreter/Interpreter.php
+++ src/interpreter/Interpreter.php
@@ -2,7 +2,9 @@
 
 namespace IPP\Student;
 
+use DOMDocument;
 use IPP\Core\AbstractInterpreter;
+use IPP\Core\ReturnCode;
 use IPP\Core\Exception\NotImplementedException;
 
 class Interpreter extends AbstractInterpreter
@@ -11,10 +13,19 @@ class Interpreter extends AbstractInterpreter
     {
         // TODO: Start your code here
         // Check IPP\Core\AbstractInterpreter for predefined I/O objects:
-        // $dom = $this->source->getDOMDocument();
+        $dom = $this->source->getDOMDocument();
+        
+        // Check if XML is valid
+        if (!$dom instanceof DOMDocument) {
+            $this->stderr->writeString("Error: Faled to load XML soruce\n");
+            return ReturnCode::INVALID_XML_ERROR;
+        }
+
+        $program = $dom->documentElement;
+
         // $val = $this->input->readString();
         // $this->stdout->writeString("stdout");
         // $this->stderr->writeString("stderr");
-        throw new NotImplementedException();
+        return 0;
     }
 }
