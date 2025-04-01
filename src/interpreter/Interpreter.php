c4906e3b49ebcc317f27fbf78b1ec82870583527
rm-a0 <michalrepcik9@gmail.com>
2025-04-01 12:53:59 +0200

    feat: implement parse program and parse class methods

--- src/interpreter/Interpreter.php
+++ src/interpreter/Interpreter.php
@@ -8,12 +8,16 @@
 namespace IPP\Student;
 
 use DOMDocument;
+use DOMElement;
 use IPP\Core\AbstractInterpreter;
 use IPP\Core\ReturnCode;
 use IPP\Core\Exception\NotImplementedException;
 
 class Interpreter extends AbstractInterpreter
 {
+    /** @var array<string, SOLClass> */
+    private array $classes = [];
+
     public function execute(): int
     {
         // TODO: Start your code here
@@ -29,7 +33,16 @@ class Interpreter extends AbstractInterpreter
             return ReturnCode::INVALID_XML_ERROR;
         }
 
-        $program = $dom->documentElement;
+        // Parse XML representation of the program
+        $parseResult = $this->parseProgram($dom->documentElement);
+        if ($parseResult != ReturnCode::OK) {
+            return $parseResult;
+        }
+
+        if(!isset($this->classes["Main"])) {
+            $this->stderr->writeString("Error: Main class not found\n");
+            return ReturnCode::PARSE_MAIN_ERROR;
+        }
 
         return ReturnCode::OK;
     }
@@ -47,5 +60,45 @@ class Interpreter extends AbstractInterpreter
             $this->stderr->writeString("Error: failed loading XML: " . $e->getMessage() . "\n");
             return null;
         }
+    }
+
+    /**
+     * Parses root of the DOM document
+     * @return int The appropriate return code
+     */
+    private function parseProgram(DOMElement $root): int
+    {
+        // Validate program element
+        if ($root->tagName != "program" || $root->getAttribute("language") != "SOL25") {
+            $this->stderr->writeString("Invalid XML format: root element must be 'program' with language='SOL25'\n");
+            return ReturnCode::INVALID_SOURCE_STRUCTURE_ERROR;
+        }
+
+        // Parse all class elements
+        foreach ($root->getElementsByTagName("class") as $classNode) {
+            $this->parseClass($classNode);
+        }
+
+        return ReturnCode::OK;
+    }
+
+    /**
+     * Parses class element of the DOM document and creates class object
+     * @return int The appropriate return code
+     */
+    private function parseClass(DOMElement $classNode): int
+    {
+        $name = $classNode->getAttribute("name");
+        $parent = $classNode->getAttribute("parent");
+
+        if (empty($name) || empty($parent)) {
+            $this->stderr->writeString("Invalid class definition: missing name or parent\n");
+            return ReturnCode::INVALID_SOURCE_STRUCTURE_ERROR;
+        }
+
+        $class = new SOLClass($name, $parent);
+
+        $this->classes[$name] = $class;
+        return ReturnCode::OK;
     }
 }
